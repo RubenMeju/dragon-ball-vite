3484e6ad68c6b2ca31cdfad00b99e8f9f51dbe82
RubenMeju <rubenmeju@outlook.es>
2023-05-17 16:52:58 +0200

    refactor usedata - service

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-
-const BASE_URL = 'http://127.0.0.1:8000/api/character/?search='
+import { fetchCharacters } from '../service/characters.service'
 
 export const useData = () => {
   const [loading, setLoading] = useState(true)
@@ -9,11 +8,10 @@ export const useData = () => {
 
   const getData = async (search = '') => {
     setLoading(true)
-    const response = await fetch(BASE_URL + search)
-    const data = await response.json()
-    console.log(data)
-    setData(data)
-    setLoading(false)
+    await fetchCharacters(search).then((data) => {
+      setData(data)
+      setLoading(false)
+    })
   }
 
   useEffect(() => {
